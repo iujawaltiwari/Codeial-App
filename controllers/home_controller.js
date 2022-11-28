4df8166b63bc18da55fbb49cb3f58edda2d9dd45
Ujawal Tiwari <ujawal0011@gmail.com>
2022-11-28 17:50:47 +0530

    Add async and await in home cantroller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
   // console.log(req.cookies);
   // res.cookie('user_id', 25);
 
@@ -12,24 +12,34 @@ module.exports.home = function (req, res) {
   //     });
   // });
 
-  // populate the user of each post
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .exec(function (err, posts) {
-      User.find({}, function (err, users) {
-        return res.render("home", {
-          title: "Codeial | Home",
-          posts: posts,
-          all_users: users,
-        });
+  try {
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
+    let users = await User.find({});
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log('Error', err);
+    return;
+  }
+
+  // populate the user of each post
 };
 
 // module.exports.actionName = function(req, res){}
+
+// using then
+// Post.find({}).populate('comment').then(function())
+
+// let post = Post.find({}).populate('comment').exec()
+
+// post.then()
