d26a8801068627bf49587d2860340710be3223bd
Ujawal Tiwari <ujawal0011@gmail.com>
2022-12-02 18:09:16 +0530

    problem to using multer

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,26 +1,63 @@
-{
-// Method to submit the form data for post using AJAYX
-  let createPost = function(){
-    let newPostForm = $('#new-post-form');
+// {
+// // Method to submit the form data for post using AJAYX
+//   let createPost = function(){
+//     let newPostForm = $('#new-post-form');
 
-    newPostForm.submit(function(e){
-        e.preventDefault();
+//     newPostForm.submit(function(e){
+//         e.preventDefault();
 
-        $.ajax({
-            type: 'post',
-            url: '/post/create',
-            data: newPostForm.serialize(),
-            dataType: "dataType",
-            success: function (data) {
-                console.log(data);
-            }, error: function(error){
-                console.log(error.responseText);
-            } 
-        });
-    });
-  }
+//         $.ajax({
+//             type: 'post',
+//             url: '/post/create',
+//             data: newPostForm.serialize(),
+//             dataType: "dataType",
+//             success: function (data) {
 
-// method to create a post in DOM
+//                 let newPost = newPostDom(data.post);
+//                 $('#post-list-container>ul').prepend(newPost);
+                
+//             }, error: function(error){
+//                 console.log(error.responseText);
+//             } 
+//         });
+//     });
+//   }
 
-  createPost();
-}
+// // method to create a post in DOM
+//   let newPostDom = function(post){
+//     return $(`<li id="post-${post._id}">
+//     <p>  
+//       <small>
+//         <a class="delete-post-button" href="/post/destroy/${ post.id }">X</a>
+//       </small>
+//       ${ post.content }
+//       <br />
+//       <small> 
+//          ${ post.user.name } 
+//        </small>
+//     </p>
+//     <div class="post-comments">
+
+//       <form action="/comments/create" method="POST">
+//         <input
+//           type="text"
+//           name="content"
+//           placeholder="Type Here to add comment..."
+//           required
+//         />
+//         <input type="hidden" name="post" value="${ post._id }" />
+//         <input type="submit" value="Add Comment" />
+//       </form>
+
+//       <div class="post-comments-list">
+//         <ul id="post-comments-${ post._id }">
+          
+//         </ul>
+//       </div>
+//     </div>
+//   </li>
+//   `)
+//   }
+
+//   createPost();
+// }
--- controllers/users_conteoller.js
+++ controllers/users_conteoller.js
@@ -9,14 +9,43 @@ module.exports.profile = function (req, res) {
   });
 };
 
-module.exports.update = function(req,res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-           return res.redirect('back');
-        });
-    }else{
+module.exports.update = async function(req,res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //        return res.redirect('back');
+    //     });
+    // }else{
+          //  req.flash('error','Unauthorized');
+    //     return res.status(401).send('Unauthorized');
+    // }
+
+     if(req.user.id == req.params.id){
+
+      try{
+
+        let user = await User.findById(req.params.id);
+        User.uploadedAvatar(req, res, function(err){
+          if(err){
+            console.log('******Multer Error', err);
+          }
+          
+          console.log(req.file);
+
+        })
+
+      }catch{
+
+        req.flash('error', err);
+        return res.redirect('back');
+
+      }
+
+     }else{
+        
+       req.flash('error','Unauthorized');
         return res.status(401).send('Unauthorized');
-    }
+
+     }
 }
 
 // render the sign up page
